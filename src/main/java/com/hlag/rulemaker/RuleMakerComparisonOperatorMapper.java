36674604246b607c63e229637aa7de03c9f64186
przemyslaw.ostrouch <przemyslaw.ostrouch2@ext.hlag.com>
2024-05-08 12:07:19 +0200

    feat(mapper): operator mapper refactor, removed names, keys for operator used as reference.

--- src/main/java/com/hlag/rulemaker/RuleMakerComparisonOperatorMapper.java
+++ src/main/java/com/hlag/rulemaker/RuleMakerComparisonOperatorMapper.java
@@ -1,5 +1,14 @@
 package com.hlag.rulemaker;
 
+import static com.hlag.rulemaker.ExtendedNumericComparisonExpression.LTE_LT;
+import static com.hlag.rulemaker.ExtendedNumericComparisonExpression.LTE_LTE;
+import static com.hlag.rulemaker.ExtendedNumericComparisonExpression.LT_LT;
+import static com.hlag.rulemaker.ExtendedNumericComparisonExpression.LT_LTE;
+import static io.github.jamsesso.jsonlogic.evaluator.expressions.NumericComparisonExpression.GT;
+import static io.github.jamsesso.jsonlogic.evaluator.expressions.NumericComparisonExpression.GTE;
+import static io.github.jamsesso.jsonlogic.evaluator.expressions.NumericComparisonExpression.LT;
+import static io.github.jamsesso.jsonlogic.evaluator.expressions.NumericComparisonExpression.LTE;
+
 import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,17 +17,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum RuleMakerComparisonOperatorMapper {
 
-  LOWER("<", "lower"),
-  LOWER_OR_EQUAL("<=", "lowerOrEqual"),
-  GREATER(">", "greater"),
-  GREATER_OR_EQUAL(">=", "greaterOrEqual"),
-  LOWER_OR_EQUAL_LOWER_OR_EQUAL("<= <=", "lte"),
-  LOWER_LOWER("< <", "lt"),
-  LOWER_OR_EQUAL_LOWER("<= <", "lteLt"),
-  LOWER_LOWER_OR_EQUAL("< <=", "ltLte");
+  LOWER(LT.key()),
+  LOWER_OR_EQUAL(LTE.key()),
+  GREATER(GT.key()),
+  GREATER_OR_EQUAL(GTE.key()),
+  LOWER_OR_EQUAL_LOWER_OR_EQUAL(LTE_LTE.key()),
+  LOWER_LOWER(LT_LT.key()),
+  LOWER_OR_EQUAL_LOWER(LTE_LT.key()),
+  LOWER_LOWER_OR_EQUAL(LT_LTE.key());
 
   private final String ruleWrightOperator;
-  private final String ruleWrightName;
 
   public static RuleMakerComparisonOperatorMapper fromRuleWrightOperator(String ruleWrightOperator) {
     return Arrays.stream(RuleMakerComparisonOperatorMapper.values())
